14519b89c1d444a0df27ac09fda4ae9be9c8afbe
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-18 22:24:36 +0200

    Add About page and fix navigation

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const homeController = require('./controllers/homeController.js');
+const cubeController = require('./controllers/cubeController.js');
 const router = express.Router();
 
 router.get('/', homeController.index);
+router.get('/about', homeController.about);
+router.use('/cube', cubeController);
 
 module.exports = router;
 
