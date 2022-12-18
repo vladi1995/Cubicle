9c7e1173ddb12c1e5319f1445ee4b7510cbd52bb
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-18 22:56:26 +0200

    Add dynamic cube rendering

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
+
 const router = express.Router();
 
 router.get('/', homeController.index);
