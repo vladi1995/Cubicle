d75c484eb39317021d4b5561fc01df2ab3639915
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-18 18:00:19 +0200

    Added src folder

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(5000, () => console.log(`App is listening on port 5000`));
