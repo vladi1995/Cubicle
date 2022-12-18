76c1f7ffdd168aed2aa83a2499da3359d0cc3f8d
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-18 18:22:54 +0200

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 app.listen(5000, () => console.log(`App is listening on port 5000`));
