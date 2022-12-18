a02f790eb7f80c7c62c41c4392ceff3691156da4
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-18 23:01:58 +0200

    Fix home controller

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,7 @@ const cubeController = require('./controllers/cubeController.js');
 
 const router = express.Router();
 
-router.get('/', homeController.index);
-router.get('/about', homeController.about);
+router.use('/', homeController);
 router.use('/cube', cubeController);
 
 module.exports = router;
