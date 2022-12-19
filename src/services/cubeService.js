2e97aa7d3f4999978605f41c95d16ace6e8a2e8c
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-19 20:57:16 +0200

    Add incremented id to created cubes.

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,7 +3,7 @@ const cubes = require('../db.json');
 const path = require('path');
 
 exports.save = (cube) => {
-    cubes.push(cube); // Does not save on the server! Data only saves on the array
+    cubes.push({id: cubes[cubes.length-1].id + 1, ...cube}); // Does not save on the server! Data only saves on the array
     let textData = JSON.stringify(cubes, '', 4);
     return fs.writeFile(path.resolve('src', 'db.json'), textData, {encoding: 'utf-8'});
 }
