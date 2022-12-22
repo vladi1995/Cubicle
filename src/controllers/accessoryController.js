dc4922428cd7e3e352e7c4057cb63c058485d69b
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2022-12-22 15:27:59 +0200

    Create post accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 router.get('/create', (req, res) => {
-    res.render('accessory/create')
+    res.render('accessory/create');
+});
+
+router.post('/create', (req, res) => {
+    
+    res.redirect('/');
 });
 
 module.exports = router;
