c3f3bd070c0c444c6e7083c5c59ed61bd160a978
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2024-01-23 11:39:17 +0200

    add, read, details, search implemented

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
-
+const db = require('../db.json');
 const Cube = require('../models/Cube');
+
+router.get('/details/:cubeId', (req, res) => {
+    const cube = db.cubes.find(el => el.id === Number(req.params.cubeId));
+
+    if (!cube) {
+        return res.redirect('/404');
+    }
+
+    res.render('details', {...cube});
+});
 
 router.get('/create', (req, res) => {
     res.render('create');
 });
 
 router.post('/create', (req, res) => {
-    const cube = new Cube(req.body);
-    
+    const {name, description, imageUrl, difficultyLevel} = req.body;
+
+    const cube = new Cube(name, description, imageUrl, difficultyLevel);
+
+    cube.save();
+
     res.redirect('/');
 });
 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../db.json');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    const {search, from, to} = req.query;
+
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        cubes = cubes.filter(cube => Number(cube.difficultyLevel) >= Number(from));
+    }
+
+    if (to) {
+        cubes = cubes.filter(cube => Number(cube.difficultyLevel) <= Number(to));
+    }
+
+    res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
     res.render('about');
+});
+
+router.get('/404', (req, res) => {
+    res.render('404');
 });
 
 module.exports = router;
